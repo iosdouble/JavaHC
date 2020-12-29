92a6bac9b740b58b4daaffd6143e590d2b6226a5
iosdouble <18202504057@163.com>
2020-12-29 15:30:35 +0800

    实例变量非线程安全问题

---
+++ src/main/java/com/thread/charp02/t2/Run.java
@@ -0,0 +1,19 @@
+package com.thread.charp02.t2;
+
+/**
+ * com.thread.charp02.t2
+ * create by admin nihui
+ * create time 2020/12/29
+ * version 1.0
+ **/
+public class Run {
+    public static void main(String[] args) {
+        HasSelfPrivateNum numRef = new HasSelfPrivateNum();
+
+        ThreadA athread = new ThreadA(numRef);
+        athread.start();
+
+        ThreadB bthread = new ThreadB(numRef);
+        bthread.start();
+    }
+}
