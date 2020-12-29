124ee79c81d72f184a402907a1741eefaa29aaa1
iosdouble <18202504057@163.com>
2020-12-29 11:55:46 +0800

    线程优先级

---
+++ src/main/java/com/thread/charp01/countPriority/Run.java
@@ -0,0 +1,13 @@
+package com.thread.charp01.countPriority;
+
+/**
+ * com.thread.charp01.countPriority
+ * create by admin nihui
+ * create time 2020/12/29
+ * version 1.0
+ **/
+public class Run {
+    public static void main(String[] args) {
+
+    }
+}
