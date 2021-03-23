a1e1c42c9e700b252eab1dbce24b32210f57a948
iosdouble <18202504057@163.com>
2021-03-23 08:48:17 +0800

    验证同步synchronized(this)代码块是锁定当前对象

---
+++ src/main/java/com/thread/charp02/synBlockString/Service.java
@@ -0,0 +1,15 @@
+package com.thread.charp02.synBlockString;
+
+/**
+ * com.thread.charp02.synBlockString
+ * create by admin nihui
+ * create time 2021/2/23
+ * version 1.0
+ **/
+public class Service {
+
+    private String usernameParam;
+    private String passwordParam;
+
+
+}
