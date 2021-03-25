054d17ac7542c1ed8ed48253d9e8f1b4473e8295
iosdouble <18202504057@163.com>
2021-03-25 17:57:31 +0800

    算法实现

---
+++ src/main/java/com/algorithm/charp01/test1_2/Test.java
@@ -0,0 +1,18 @@
+package com.algorithm.charp01.test1_2;
+
+import com.algorithm.charp01.linklist.LNode;
+
+/**
+ * com.algorithm.charp01.test1_2
+ * create by admin nihui
+ * create time 2021/3/25
+ * version 1.0
+ * TODO 如何从无序链表中移除重复项
+ **/
+public class Test {
+    public static void removeDup(LNode head){
+        if (head == null || head.next == null){
+
+        }
+    }
+}
