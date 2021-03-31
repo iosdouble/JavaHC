d00441d60bf17ec2d535dbb1113f93e7903d1de9
iosdouble <18202504057@163.com>
2021-03-31 10:55:05 +0800

    算法实现

--- src/main/java/com/algorithm/charp01/test1_2/Test.java
+++ src/main/java/com/algorithm/charp01/test1_2/Test.java
@@ -12,7 +12,24 @@ import com.algorithm.charp01.linklist.LNode;
 public class Test {
     public static void removeDup(LNode head){
         if (head == null || head.next == null){
+            return;
+        }
 
+        LNode outerCur = head.next; // 用于外层循环，指向链表第一个节点
+        LNode innerCur = null; // 内层循环用来遍历outerCur后面的节点
+        LNode innerPre = null; // innerCur 的前驱结点
+
+        for (;outerCur!=null;outerCur = outerCur.next){
+            for (innerCur = outerCur.next,innerPre= outerCur;innerCur!=null;){
+                // 找到重复的节点并删除
+                if (outerCur.data == innerCur.data){
+                    innerPre.next = innerCur.next;
+                    innerCur = innerCur.next;
+                }else {
+                    innerPre = innerCur;
+                    innerCur = innerCur.next;
+                }
+            }
         }
     }
 }
